7af1b2989b878597de1483b56048e279ae65865d
Fritz Schmude <mail@fritz-schmude.de>
2015-05-02 18:42:49 +0200

    .

---
+++ V/VAdminBild.php
@@ -0,0 +1,55 @@
+<?
+/**
+ * edit form 1 article
+ */
+require_once 'V/VAdmin.php';
+
+class VAdminBild extends VAdmin {
+  
+  /**
+   * display the edit form for a single image
+   * @param string $errmsg
+   * @param array $data = array(
+   *  );
+   */
+  public function display($errmsg, $data) {
+    $this->displayHead($errmsg, 'Bild editieren');
+    ?>
+    aid = <?= $data['id'] ?>
+    <form method="post" action="admin.php">
+    <input type="hidden" name="mode" value="Bild_up">
+    <input type="hidden" name="id" value="<?= $data['id'] ?>">
+    Breite:
+    <input type="text" name="width" value="<?= $data['width'] ?>" style="width:100px">
+    <br>
+    Höhe:
+    <input type="text" name="height" value="<?= $data['height'] ?>" style="width:100px">
+    <br>
+    Extension (Dateinamen-Endung):
+    <input type="text" name="ext" value="<?= $data['ext'] ?>" style="width:100px">
+    <br>
+    
+    <input type="submit" value="Bilddaten ändern">
+    
+    </form>
+
+    <form action="admin.php" method="post">
+    <input type="hidden" value="Bild_list" name="mode"></input>
+    <div align="center">
+    <input type="submit" value="Bilder verwalten">
+    </div>
+    </form>
+
+    <form action="admin.php" method="post">
+    <input type="hidden" value="Artikel_list" name="mode"></input>
+    <div align="center">
+    <input type="submit" value="Artikel verwalten">
+    </div>
+    </form>
+    
+    <?
+    $this->displayFoot();
+  }
+
+}
+
