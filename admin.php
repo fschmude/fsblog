1f36010bbf580b67e4d095640267fd7731f7a1e6
Fritz Schmude <mail@fritz-schmude.de>
2015-05-02 18:42:50 +0200

    .

--- admin.php
+++ admin.php
@@ -1,16 +1,9 @@
 <?
 session_start();
 
-echo __LINE__.'<br>';
-flush();
 require_once 'config.php';
-echo __LINE__.'<br>';
-flush();
 require_once 'C/CAdmin.php';
-echo __LINE__.'<br>';
-flush();
 
 $c = new CAdmin();
-echo __LINE__.'<br>';
-flush();
 $c->run($_GET, $_POST);
+
