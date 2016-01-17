e106c65503705822d8c16a4efd20bf9f90d9bfea
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- kontakt.php
+++ kontakt.php
@@ -1,9 +1,6 @@
 <?
-require_once 'M/MArtikel.php';
-require_once 'V/VStatic.php';
 require_once 'C/CStatic.php';
 
-$mart = new MArtikel();
-$v = new VStatic();
 $c = new CStatic();
-$c->work($mart, $v, 'kontakt');
+$c->work(array('page' => 'kontakt'), 0, 0);
+
