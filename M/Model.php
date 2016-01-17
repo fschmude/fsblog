032ea8c3684d064bda1233de2d28f6db326a7b40
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- M/Model.php
+++ M/Model.php
@@ -1,17 +1,5 @@
 <?
-require_once 'config.php';
-
-class Model {
+abstract class Model {
   
-  /**
-   * Get the DB connection handle
-   */
-  public function get_pdo() {
-    if (!isset($GLOBALS['pdo']) || !$GLOBALS['pdo']) {
-      $GLOBALS['pdo'] = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
-    }
-    return $GLOBALS['pdo'];
-  }
-   
 }
 
