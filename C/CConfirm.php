6805c9631495017bf817f6e1a5366e354eb722d8
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- C/CConfirm.php
+++ C/CConfirm.php
@@ -1,19 +1,29 @@
 <?
-require_once 'config.php';
-require_once 'M/Email.php';
+/**
+ * Controller für alle Leser-Interaktionen 
+ */
+require_once 'C/CController.php';
 require_once 'M/MLeser.php';
-class CConfirm {
+
+define('CONFIRM_DISPLAYMODE_NOTHING', 0);
+define('CONFIRM_DISPLAYMODE_POSTMAIL', 1);
+define('CONFIRM_DISPLAYMODE_POSTCONFIRM', 2);
+define('CONFIRM_DISPLAYMODE_LMAIL', 3);
+define('CONFIRM_DISPLAYMODE_LCONFIRM', 4);
+
+class CConfirm extends CController {
   
-  public function work($get, $post, $mart, $mleser, $view) {
-    $errmsg = $msg = '';
-    $misc = array();
+  public function work($get, $post, $files) {
+    $errmsg = $msg = $lmail = $usermail = $aurl = '';
     $navi_arts = array();
     
     try {
       $b_input_wellformed = false;
       $titel = '';
-      $displaymode = VCONFIRM_DISPLAYMODE_NOTHING;
+      $displaymode = CONFIRM_DISPLAYMODE_NOTHING;
       $Mail = new Email();
+      $mart = $this->getObject('MArtikel');
+      $mleser = $this->getObject('MLeser');
       
       // new post?
       if (isset($post['aid']) && ($aid = (int) $post['aid'])
@@ -25,20 +35,17 @@ class CConfirm {
         $titel = 'Kommentar erfasst';
         if (!strlen($usermail = trim($post['usermail']))) {
           $msg = 'Keine E-Mail-Adresse angegeben.';
-        } elseif (!$Mail->validate_address($usermail)) {
+        } elseif (!$Mail->validateAddress($usermail)) {
           $msg = '"'.$usermail.'" scheint keine gültige E-Mail-Adresse zu sein.';
         } elseif (!strlen($ptext = trim($post['ptext']))) {
           $msg = 'Es wurde kein Posting eingetippt.';
         } else {
           // ok, do something
-          if ($mart->create_post($aid, trim($post['username']), $usermail, $ptext)) {
-            $displaymode = VCONFIRM_DISPLAYMODE_POSTMAIL;
+          if ($mart->createPost($aid, trim($post['username']), $usermail, $ptext)) {
+            $displaymode = CONFIRM_DISPLAYMODE_POSTMAIL;
           }
         }
-        $misc = array(
-          'aurl' => $mart->getUrl($aid),
-          'usermail' => $usermail
-        );
+        $aurl = $mart->getUrl($aid);
       }
       
       // confirm post?
@@ -48,12 +55,10 @@ class CConfirm {
         if (!strlen($code = trim($get['code']))) {
           $msg = 'Es wurde kein Bestätigungs-Code angegeben.';
         } else {
-          $pinfo = $mart->confirm_post($pid, $code);
-          $displaymode = VCONFIRM_DISPLAYMODE_POSTCONFIRM;
-          $misc = array(
-            'url' => $mart->getUrl($pinfo['aid']),
-            'usermail' => $pinfo['usermail']
-          );
+          $pinfo = $mart->confirmPost($pid, $code);
+          $displaymode = CONFIRM_DISPLAYMODE_POSTCONFIRM;
+          $aurl = $mart->getUrl($pinfo['aid']);
+          $usermail = $pinfo['usermail'];
         }
       }
       
@@ -64,12 +69,11 @@ class CConfirm {
         $lmail = trim($post['lmail']);
         if (!$lmail) {
           $msg = 'Keine E-Mail-Adresse übergeben';
-        } elseif (!$Mail->validate_address($lmail)) {
+        } elseif (!$Mail->validateAddress($lmail)) {
           $msg = '"'.$lmail.'" ist keine gültige E-Mail-Adresse.';
         } else {
-          $mleser->create_leser($lmail);
-          $displaymode = VCONFIRM_DISPLAYMODE_LMAIL;
-          $misc = array('lmail' => $lmail);
+          $mleser->createLeser($lmail);
+          $displaymode = CONFIRM_DISPLAYMODE_LMAIL;
         } 
       }
       
@@ -78,8 +82,7 @@ class CConfirm {
         $b_input_wellformed = true;
         $titel = 'Mailadresse bestätigt';
         $lmail = $mleser->confirm($code);
-        $displaymode = VCONFIRM_DISPLAYMODE_LCONFIRM;
-        $misc = array('lmail' => $lmail);
+        $displaymode = CONFIRM_DISPLAYMODE_LCONFIRM;
       }
       
       if (!$b_input_wellformed) {
@@ -87,14 +90,24 @@ class CConfirm {
       }
       
       // navi_arts werden in jedem Fall gebraucht
-      $navi_arts = $mart->get_top(3);
+      $navi_arts = $mart->getTop(3);
       
     } catch (Exception $e) {
       $errmsg = $e->getMessage();
     }
 
     // now display the whole thing
-    $view->display($errmsg, $msg, $titel, $navi_arts, $displaymode, $misc);
+    $view = $this->getObject('VConfirm');
+    $view->display($errmsg, array(
+      'msg' => $msg,
+      'titel' => $titel,
+      'navi_arts' => $navi_arts,
+      'displaymode' => $displaymode,
+      'usermail' => $usermail,
+      'lmail' => $lmail,
+      'aurl' => $aurl
+    ));
   }
   
 }
+
