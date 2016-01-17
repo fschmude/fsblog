9fb0276601913ee9d08cde3a3eeabc4cb7ac825d
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- mailtext.php
+++ mailtext.php
@@ -2,9 +2,8 @@
 // zeige einen Vorschlagstext für das Teaser-Mail an alle Abonnenten
 session_start();
 
-require_once 'config.php';
 require_once 'C/CMailtext.php';
 
 $c = new CMailtext();
-$c->run($_GET, $_POST, $_FILES);
+$c->work($_GET, $_POST, $_FILES);
 
