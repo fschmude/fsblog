27c54c2c49d4e994450b8c921f2f32cda4ce2761
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- confirm.php
+++ confirm.php
@@ -1,12 +1,6 @@
 <?
 require_once 'C/CConfirm.php';
-require_once 'M/MArtikel.php';
-require_once 'M/MLeser.php';
-require_once 'V/VConfirm.php';
 
-$mart = new MArtikel();
-$mleser = new MLeser();
-$v = new VConfirm();
 $c = new CConfirm();
-$c->work($_GET, $_POST, $mart, $mleser, $v);
+$c->work($_GET, $_POST, 0);
 
