38c6056e1df4f25853bc834658be87f059b8a568
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- V/VConfirm.php
+++ V/VConfirm.php
@@ -1,10 +1,4 @@
 <?
-define('VCONFIRM_DISPLAYMODE_NOTHING', 0);
-define('VCONFIRM_DISPLAYMODE_POSTMAIL', 1);
-define('VCONFIRM_DISPLAYMODE_POSTCONFIRM', 2);
-define('VCONFIRM_DISPLAYMODE_LMAIL', 3);
-define('VCONFIRM_DISPLAYMODE_LCONFIRM', 4);
-
 require_once 'V/View.php';
 
 class VConfirm extends View {
@@ -12,13 +6,13 @@ class VConfirm extends View {
   /**
    * Zentrale Anzeigefunktion
    */
-  public function display($errmsg, $msg, $titel, $navi_arts, $displaymode, $misc) {
+  public function display($errmsg, $vdata) {
     $content = '';
-    switch ($displaymode) {
-    case VCONFIRM_DISPLAYMODE_POSTMAIL:
+    switch ($vdata['displaymode']) {
+    case CONFIRM_DISPLAYMODE_POSTMAIL:
       $content = 'Danke für Ihren Kommentar.'
         .'<br><br>'."\n"
-        .'Sie erhalten in Kürze eine E-Mail an "'.$misc['usermail'].'" mit einem Bestätigungslink.'
+        .'Sie erhalten in Kürze eine E-Mail an "'.$vdata['usermail'].'" mit einem Bestätigungslink.'
         .'<br><br>'."\n"
         .'Bitte sehen Sie in Ihr Postfach, um diesen Link zu aktivieren.'
         .'<br><br>'."\n"
@@ -27,16 +21,16 @@ class VConfirm extends View {
       ;
       break;
       
-    case VCONFIRM_DISPLAYMODE_POSTCONFIRM:
-      $content = 'Die von Ihnen angegebene E-Mail-Adresse "'.$misc['usermail'].'" wurde bestätigt.'
+    case CONFIRM_DISPLAYMODE_POSTCONFIRM:
+      $content = 'Die von Ihnen angegebene E-Mail-Adresse "'.$vdata['usermail'].'" wurde bestätigt.'
         .'<br><br>'."\n"
         .'Sobald Ihr Kommentar inhaltlich geprüft ist, wird er freigeschaltet.'
         .'<br><br>'."\n"
       ;  
       break;
       
-    case VCONFIRM_DISPLAYMODE_LMAIL:
-      $content = 'Die von Ihnen angegebene E-Mail-Adresse "'.$misc['lmail'].'" wurde vorläufig eingetragen.'
+    case CONFIRM_DISPLAYMODE_LMAIL:
+      $content = 'Die von Ihnen angegebene E-Mail-Adresse "'.$vdata['lmail'].'" wurde vorläufig eingetragen.'
         .'<br><br>'."\n"
         .'Sie erhalten in Kürze eine E-Mail.'
         .'<br><br>'."\n"
@@ -45,37 +39,37 @@ class VConfirm extends View {
       ;
       break;
       
-    case VCONFIRM_DISPLAYMODE_LCONFIRM:
-      $content = 'Ihre E-Mail-Adresse "'.$misc['lmail'].'" wurde bestätigt.'
+    case CONFIRM_DISPLAYMODE_LCONFIRM:
+      $content = 'Ihre E-Mail-Adresse "'.$vdata['lmail'].'" wurde bestätigt.'
         .'<br><br>'."\n"
         .'Sie werden nun bei allen neuen Artikeln auf fs-blog.de benachrichtigt.  '
         .'<br><br>'."\n"
       ;
       break;
       
-    case VCONFIRM_DISPLAYMODE_NOTHING:
+    case CONFIRM_DISPLAYMODE_NOTHING:
       break;
       
     default:
-      $errmsg = 'Ungültiger display mode: '.$displaymode;
+      $errmsg = 'Ungültiger display mode: '.$vdata['displaymode'];
     }
     
-    $this->head($titel, '', '', '');
+    $this->head($vdata['titel'], '', '', '');
     
     if ($errmsg) {
       $this->errmsg($errmsg);
       
     } else {
-      if ($msg) {
-        echo $msg;
+      if ($vdata['msg']) {
+        echo $vdata['msg'];
         echo '<br><br>'."\n";
       }
       if ($content) {
         echo $content;
       }
       
-      if (isset($misc['aurl']) && $misc['aurl']) {
-        echo '<a href="artikel/'.$misc['aurl'].'.htm">Zurück zum Artikel</a>';
+      if (isset($vdata['aurl']) && $vdata['aurl']) {
+        echo '<a href="artikel/'.$vdata['aurl'].'.htm">Zurück zum Artikel</a>';
         echo '<br><br>';
       }
       ?>
@@ -83,6 +77,8 @@ class VConfirm extends View {
       <?
     }
     
-    $this->foot($navi_arts);
+    $this->foot($vdata['navi_arts']);
   }
+  
 }
+
