b2c25383069d00ccad334cd17e609af684dda977
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- about.php
+++ about.php
@@ -1,9 +1,6 @@
 <?
-require_once 'M/MArtikel.php';
-require_once 'V/VStatic.php';
 require_once 'C/CStatic.php';
 
-$mart = new MArtikel();
-$v = new VStatic();
 $c = new CStatic();
-$c->work($mart, $v, 'about');
+$c->work(array('page' => 'about'), 0, 0);
+
