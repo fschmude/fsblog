eff5c98c981e297b6916265ba63a0a617560a794
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- M/MHelper.php
+++ M/MHelper.php
@@ -7,7 +7,7 @@ class MHelper {
   /**
    * Produce a new code
    */
-  public function make_code() {
+  public function makeCode() {
     $code = '';
     for ($i=1;$i<=15;$i++) {
       $code .= chr(rand(65,90));
