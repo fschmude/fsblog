49d185f153ad7326f7a38b5ad5bee656159ff861
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:16 +0100

    .

---
+++ tests/MBild_Test.php
@@ -0,0 +1,33 @@
+<?
+require_once 'fslib.php';
+require_once 'Testcase.php';
+require_once 'M/MBild.php';
+
+class MBild_Test extends Testcase {
+  
+  /**
+   * test editing
+   */
+  public function test_01() {
+    $m = new MBild;
+    $id = $m->create();
+    $row = array(
+      'id' => $id,
+      'width' => '99',
+      'height' => '99',
+      'url' => 'abc',
+      'ext' => 'tst',
+      'alt' => 'Haha'
+    );
+    $upfile = array('size' => 100, 'tmp_name' => '1.tst');
+    $m->edit($row, $upfile);
+    $this->check_db(
+      "SELECT * FROM bilder WHERE id=".$id,
+      $row
+    );
+  }
+  
+
+  
+}
+
