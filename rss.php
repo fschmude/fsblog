1754839df8c8c2e6bd8cb6dcf161611b2dffe738
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- rss.php
+++ rss.php
@@ -1,10 +1,6 @@
 <?
-require_once 'M/MArtikel.php';
 require_once 'C/CRss.php';
-require_once 'V/VRss.php';
 
-$m = new MArtikel();
-$v = new VRss();
 $c = new CRss();
-$c->work($m, $v);
+$c->work(0, 0, 0);
 
