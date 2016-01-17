ee7d4cafb8f7a18b2dfb9005bc5360cc7e85fc12
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- C/CStatic.php
+++ C/CStatic.php
@@ -1,24 +1,31 @@
 <?
-require_once 'config.php';
+require_once 'C/CController.php';
 
-class CStatic {
+class CStatic extends CController {
   
-  public function work($mart, $view, $page) {
-    $errmsg = '';
+  public function work($get, $post, $files) {
+  //public function work($mart, $view, $page) {
+    $errmsg = $navi_arts = '';
     try {
+      if (!$page = $get['page']) {
+        throw new Exception('No page given');
+      }
       if (!in_array($page, array('about', 'kontakt'))) {
         throw new Exception('Ungültige page: "'.$page.'"');
       }
       
       // navi_arts werden in jedem Fall gebraucht
-      $navi_arts = $mart->get_top(3);
+      $mart = $this->getObject('MArtikel');
+      $navi_arts = $mart->getTop(3);
       
     } catch (Exception $e) {
       $errmsg = $e->getMessage();
     }
     
     // display
-    $view->display($errmsg, $navi_arts, $page);
+    $view = $this->getObject('VStatic');
+    $view->display($errmsg, array('navi_arts' => $navi_arts, 'page' => $page));
   }
   
 }
+
