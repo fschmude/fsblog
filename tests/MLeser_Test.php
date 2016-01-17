0e631d4cb4cd11decb37acccfebed22da557055d
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- tests/MLeser_Test.php
+++ tests/MLeser_Test.php
@@ -20,7 +20,7 @@ class MLeser_Test extends Testcase {
       "DELETE FROM leser WHERE lmail='huhu'"
     ));
     $m = new MLeser();
-    $m->create_leser('huhu');
+    $m->createLeser('huhu');
     $row = $this->check_db(
       "SELECT DATE_FORMAT(datum, '%Y%m%d') datum, status FROM leser WHERE lmail='huhu'",
       array('datum' => Date('Ymd'), 'status' => '0')
@@ -72,7 +72,6 @@ class MLeser_Test extends Testcase {
   public function test_cfm_03() {
     // everything ok, confirm address, change status
     $m = new MLeser();
-    $pdo = $m->get_pdo();
     $this->exec_sqls(array(
       "DELETE FROM leser WHERE lmail='huhu' OR code='12a'",
       "INSERT INTO leser(lmail,    datum, code,status)"
@@ -88,7 +87,7 @@ class MLeser_Test extends Testcase {
   
   
   /**
-   * getTeaser()
+   * getMaildata()
    */
   public function test_gt1() {
     $m = new MLeser();
@@ -97,22 +96,39 @@ class MLeser_Test extends Testcase {
     ));
     $msg = '';
     try {
-      $m->getTeaser(1);
+      $m->getMaildata(1);
       $msg = 'Error not thrown!';
     } catch (Exception $e) {
-      $this->assertSame('Es existiert kein Artikel mit aid=1', $e->getMessage());
+      $this->assertSame('Kein Datensatz Nr. 1 in artikel', $e->getMessage());
     }
     if ($msg) $this->fail('Error not thrown');
   }
   
   public function test_gt2() {
+    // all ok
     $m = new MLeser();
     $this->exec_sqls(array(
       "DELETE FROM artikel WHERE id=1",
-      "INSERT INTO artikel(id,titel,text,status) VALUES(1,'Testtitel', 'Dies ist ein Testartikel',1)"
+      "INSERT INTO artikel(id,titel,text,status) VALUES(1,'Testtitel', 'Dies ist ein Testartikel',1)",
+      "DELETE FROM leser WHERE id IN(1,2) OR lmail IN('m1','m2')",
+      "INSERT INTO leser(id,lmail,code,datum,status) VALUES(1,'m1','123','1969-03-29 23:45:00', 1)",
+      "INSERT INTO leser(id,lmail,code,datum,status) VALUES(2,'m2','123','1969-03-29 23:45:00', 0)"
     ));
-    $a = $m->getTeaser(1);
+    $a = $m->getMaildata(1);
     $this->assertSame('Testtitel', $a['titel']);
+    $b1Found = $b2Found = false;
+    $aLeser = explode(',', $a['leser']);
+    foreach ($aLeser as $lmail) {
+      if ($lmail == 'm1') {
+        $b1Found = true;
+      }
+      if ($lmail == 'm2') {
+        $b2Found = true;
+      }
+    }
+    $this->assertTrue($b1Found, 'Aktiver Leser nicht in Ergebnismenge!');
+    $this->assertFalse($b2Found, 'Unbestätigter Leser in Ergebnismenge!');
   }
   
 }
+
