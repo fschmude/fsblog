b84e8fd9746cafa70237f64af176f4c5c12250ca
fschmude <mail@fritz-schmude.de>
2016-01-17 16:17:15 +0100

    .

--- M/MBild.php
+++ M/MBild.php
@@ -1,40 +1,31 @@
 <?
+require_once 'D/DBilder.php';
 require_once 'M/Model.php';
 
 class MBild extends Model {
+  
+  private $dobj = null;
+  
+  /**
+   * Konstruktor
+   */
+  public function __construct() {
+    $this->dobj = new DBilder;
+  }
   
   /**
    * Genau ein Bild anhand der ID holen
    */
   public function getItem($id) {
-    $stmt = $this->get_pdo()->prepare(
-      "SELECT *"
-      ." FROM bilder"
-      ." WHERE id=:id"
-    );
-    if (!$stmt->execute(array(':id' => $id))) {
-      throw new Exception('Fehler beim Suchen von Bild Nr. '.$id);
-    }
-    $res = $stmt->fetch(PDO::FETCH_ASSOC);
-    return $res;
+    return $this->dobj->getRow($id);
   }
   
   
   /**
-   * Ein Bild-Datensatz editieren
+   * Ein Bild-Datensatz erzeugen
    */
   public function create() {
-    $sql = "INSERT INTO bilder VALUE()";
-    $stmt = $this->get_pdo()->prepare($sql);
-    if (!$stmt->execute()) {
-      throw new Exception('Fehler bei '.$sql);
-    }
-    
-    // get ID
-    $id = $this->get_pdo()->lastInsertId();
-    if (! (int) $id) {
-      throw new Exception('Bild-id konnte nicht ermittelt werden');
-    }
+    $id = $this->dobj->create();
     return $id;
   }
   
@@ -42,29 +33,13 @@ class MBild extends Model {
   /**
    * Ein Bild-Datensatz editieren
    */
-  public function edit($row, $files) {
-    if (!isset($row['id']) || !(int)$row['id']) {
-      throw new Exception('Ungültige ID beim Editieren eines Bildes');
-    }
-    
-    $stmt = $this->get_pdo()->prepare(
-      "UPDATE bilder SET width=:width, height=:height, url=:url, ext=:ext, alt=:alt "
-      ." WHERE id=:id"
-    );
-    if (!$stmt->execute(array(
-        ':id' => $row['id'],
-        ':width' => $row['width'],
-        ':height' => $row['height'],
-        ':url' => $row['url'],
-        ':ext' => $row['ext'],
-        ':alt' => $row['alt']
-    ))) {
-      throw new Exception('Fehler beim Editieren des Bildes mit id='.$row['id']);
-    }
-    
+  public function edit($row, $upfile) {
+    // edit record
+    $this->dobj->edit($row);
+
     // handle file
-    if ($files['datei']['size']) {
-      move_uploaded_file($files['datei']['tmp_name'], 'imga/'.$row['id'].'.'.$row['ext']);
+    if ($upfile['size']) {
+      move_uploaded_file($upfile['tmp_name'], 'imga/'.$row['id'].'.'.$row['ext']);
     }
     return true;
   }
@@ -74,6 +49,9 @@ class MBild extends Model {
    * Ein Bild löschen
    */
   public function delete($id) {
+    if (!$id = (int) $id) {
+      throw new Exception('Keine Bild-ID gegeben');
+    }
     $res = $this->getItem($id);
     $file = 'imga/'.$id.'.'.$res['ext'];
     
@@ -83,12 +61,7 @@ class MBild extends Model {
     }
     
     // ds löschen
-    $stmt = $this->get_pdo()->prepare(
-      "DELETE FROM bilder WHERE id=:id"
-    );
-    if (!$stmt->execute(array(':id' => $id))) {
-      throw new Exception('Fehler beim Löschen des Bildes Nr. '.$id);
-    }
+    $this->dobj->delete($id);
   }
   
   
@@ -96,15 +69,7 @@ class MBild extends Model {
    * Alle Bild-Datensätze holen
    */
   public function getList() {
-    $stmt = $this->get_pdo()->prepare(
-      "SELECT *"
-      ." FROM bilder"
-      ." ORDER BY id"
-    );
-    if (!$stmt->execute()) {
-      throw new Exception('Fehler beim Holen aller Bilder');
-    }
-    $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $res = $this->dobj->getAll();
     return $res;
   }
   
@@ -127,20 +92,11 @@ class MBild extends Model {
    */
   public function getImageInfo($url) {
     // Zahl oder Text-Url
-    if ((int) $url) {
-      $sql = "SELECT * FROM bilder WHERE id=:id";
-      $pms = array(':id' => $url);
+    if (is_int($url)) {
+      $ret = $this->dobj->getRow($url);
     } else {
-      $sql = "SELECT * FROM bilder WHERE url=:url";
-      $pms = array(':url' => $url);
+      $ret = $this->dobj->getByUrl($url);
     }
-    
-    // now get row from db
-    $stmt = $this->get_pdo()->prepare($sql);
-    if (!$stmt->execute($pms)) {
-      throw new Exception('Fehler beim Suchen nach '.$url);
-    }
-    $ret = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!$ret) {
       throw new Exception('Kein Datensatz zu Bild-URL "'.$url.'"');
     }
