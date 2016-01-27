3234e40c43eb8f63c0ff4de650b21a2cb343a34f
fschmude <mail@fritz-schmude.de>
2016-01-27 21:42:40 +0100

    .

--- C/CArtikel.php
+++ C/CArtikel.php
@@ -4,32 +4,33 @@ require_once 'C/Controller.php';
 class CArtikel extends Controller {
   
   public function work($get, $post, $files) {
-    $errmsg = $artikel = $navi_arts = '';
+    $errmsg = $vdata = '';
     try {
       $mart = $this->getObject('MArtikel');
       $view = $this->getObject('VArtikel');
       if (isset($get['aid']) && $aid = (int) $get['aid']) {
         // call by article id
-        $artikel = $mart->getArtikelKomplett($aid);
+        $vdata = $mart->getUrl($aid);
+        $view = $this->getObject('VRedirect');
         
       } elseif (isset($get['url']) && strlen($fakeurl = trim($get['url']))) {
         // call by (faked) url?
         $artikel = $mart->getArtikelKomplettByUrl($fakeurl);
+        $navi_arts = $mart->getTop(3);
+        $vdata = array(
+          'artikel' => $artikel,
+          'navi_arts' => $navi_arts
+        );
       } else {
         throw new Exception('incorrect call');
       }
 
-      // navi_arts werden in jedem Fall gebraucht
-      $navi_arts = $mart->getTop(3);
-      
     } catch (Exception $e) {
       $errmsg = $this->handleError($e);
     }
   
-    $view->display($errmsg, array(
-      'artikel' => $artikel,
-      'navi_arts' => $navi_arts
-    ));
+    $view->display($errmsg, $vdata);
   }
   
 }
+
