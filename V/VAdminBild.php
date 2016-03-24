d09b24d6d7eb78a40a9d2bfeff611d4026525f95
fschmude <mail@fritz-schmude.de>
2016-03-24 20:55:58 +0100

    .

--- V/VAdminBild.php
+++ V/VAdminBild.php
@@ -14,59 +14,51 @@ class VAdminBild extends VAdmin {
    */
   public function display($errmsg, $data) {
     $this->displayHead($errmsg, 'Bild editieren');
+
+    $this->displayNaviLink('Bild_list', 'Bilder verwalten');
+    echo '<br>'."\n";
+
+    $bild = $data['item'];
     ?>
-    Bild id = <?= $data['id'] ?>
+    Bild id = <?= $bild['id'] ?>
     <br>
     Aktuelles Bild (hier evtl. verkleinert):
     <br>
-    <img width="<?= $data['t_width'] ?>" height="<?= $data['t_height'] ?>" src="<?= BASEURL ?>imga/<?= $data['url'] ?>.<?= $data['ext'] ?>">
+    <img width="<?= $bild['t_width'] ?>" height="<?= $bild['t_height'] ?>" src="<?= BASEURL ?>imga/<?= $bild['url'] ?>.<?= $bild['ext'] ?>">
     <br>
+
+    <form method="post" action="admin.php" enctype="multipart/form-data">
     Bilddatei uploaden, bzw. ändern:
     <input type="file" name="datei" value="" style="width:300px">
-    
     <hr>
     
     Möglichst nicht editieren (wird aus upload berechnet):
-    <form method="post" action="admin.php" enctype="multipart/form-data">
+    <br>
     <input type="hidden" name="mode" value="Bild_up">
-    <input type="hidden" name="id" value="<?= $data['id'] ?>">
+    <input type="hidden" name="id" value="<?= $bild['id'] ?>">
     Breite:
-    <input type="text" name="width" value="<?= $data['width'] ?>" style="width:100px">
+    <input type="text" name="width" value="<?= $bild['width'] ?>" style="width:100px">
     <br>
     Höhe:
-    <input type="text" name="height" value="<?= $data['height'] ?>" style="width:100px">
+    <input type="text" name="height" value="<?= $bild['height'] ?>" style="width:100px">
     <br>
     Extension (Dateinamen-Endung):
-    <input type="text" name="ext" value="<?= $data['ext'] ?>" style="width:100px">
+    <input type="text" name="ext" value="<?= $bild['ext'] ?>" style="width:100px">
     
     <hr>
 
     Ab hier editieren:
     <br>
     URL (ohne Endung):
-    <input type="text" name="url" value="<?= $data['url'] ?>" style="width:200px">
+    <input type="text" name="url" value="<?= $bild['url'] ?>" style="width:200px">
     <br>
     Beschreibung (für alt-Attribut):
-    <input type="text" name="alt" value="<?= $data['alt'] ?>" style="width:200px">
+    <input type="text" name="alt" value="<?= $bild['alt'] ?>" style="width:200px">
     <br>
     
     <input type="submit" value="Bilddaten ändern">
     </form>
 
-    <form action="admin.php" method="post">
-    <input type="hidden" value="Bild_list" name="mode"></input>
-    <div align="center">
-    <input type="submit" value="Bilder verwalten">
-    </div>
-    </form>
-
-    <form action="admin.php" method="post">
-    <input type="hidden" value="Artikel_list" name="mode"></input>
-    <div align="center">
-    <input type="submit" value="Artikel verwalten">
-    </div>
-    </form>
-    
     <?
     $this->displayFoot();
   }
