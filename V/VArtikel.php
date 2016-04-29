ac82070ff89fd362bf388863c6a5c5fa0fd7d112
fschmude <mail@fritz-schmude.de>
2016-04-29 22:10:30 +0200

    .

--- V/VArtikel.php
+++ V/VArtikel.php
@@ -3,12 +3,37 @@ require_once 'V/View.php';
 
 class VArtikel extends View {
 
-  public function display($errmsg, $ddata) {
+  /**
+   * @param string $errmsg
+   * @param array $data = array(
+   *    'type' => artikel / monat / snippet
+   *    'id' => 52, (nur bei Artikeln und Snippets)
+   *    'titel' => 'Links von März',
+   *    'url' => 'http://...komplett...',
+   *    'metadesc' => 'Links und Kommentare vom März 2016',
+   *    'datum' => '2016-04-03 11:50:17',
+   *    'text' => noch nicht nach Html geparst,
+   *    'bilder' => array, indices sind die IDs der gezogenen Bilder,
+   *    'vids' => dito,
+   *    'posts' => array der posts, (vorerst nur bei Artikeln)
+   *    'urlFB' => Facebook-URL (nur bei snippets),
+   *    'urlAfter'  => Komplett-URL des späteren Eintrags (nur bei snippets),
+   *    'urlBefore' => Komplett-URL des vorigen Eintrags (nur bei snippets),
+   *    'urlMonat' => Komplett-URL des vorigen Eintrags (nur bei snippets),
+   *    'navi_arts' => array( array() )
+   * );
+   */
+  public function display($errmsg, $data) {
+    if ($errmsg) {
+      $this->errmsg($errmsg);
+    }
+    
     try {
-      $art = $ddata['artikel'];
-      $canonical = $this->completeUrl($art['url']);
-      $this->head($art['titel'], $canonical, $art['datum'], $art['metadesc']);
-      $text_html = $this->parseArtikel($art['text'], $art['bilder'], $art['videos']);
+      if (!in_array($data['type'], array('artikel', 'monat', 'snippet'))) {
+        throw new Exception('Unbekannter Typ: "'.$data['type'].'"');
+      }
+      $this->head($data['titel'], $data['url'], $data['datum'], $data['metadesc']);
+      $text_html = $this->parseArtikel($data['text'], $data['bilder'], $data['vids']);
       
     } catch (Exception $e) {
       $errmsg = $e->getMessage();
@@ -16,10 +41,9 @@ class VArtikel extends View {
     
     if ($errmsg) {
       $this->errmsg($errmsg);
-      
-    } else {
-      echo $text_html;
     }
+    
+    echo $text_html;
     
     // Social Media Links
     ?>
@@ -44,85 +68,102 @@ class VArtikel extends View {
     <td style="width:20%">
     <?
     // Fratzenbuch
-    echo '<div class="fb-like" data-href="'.$canonical.'" data-send="false" data-layout="button_count" data-width="200" data-show-faces="true" data-font="arial"></div>';
+    echo '<div class="fb-like" data-href="'.$data['url'].'" data-send="false" data-layout="button_count" data-width="200" data-show-faces="true" data-font="arial"></div>';
     ?>
     </td></tr></table>
     <?
+    
+    // Links für snippets
+    if ($data['type'] == 'snippet') {
+      if (isset($data['urlFB']) && $data['urlFB']) {
+        echo '<a href="'.$data['urlFB'].'" target="_blank">Diesen Eintrag bei Facebook kommentieren</a><br><br>'."\n";
+      }
+      if (isset($data['urlBefore']) && $data['urlBefore']) {
+        echo '<a href="'.$data['urlBefore'].'">Voriger Eintrag</a> &lt;- &nbsp; ';
+      }
+      echo ' <a href="'.$data['urlMonat'].'">Ganzen Monat zeigen</a> ';
+      if (isset($data['urlAfter']) && $data['urlAfter']) {
+        echo ' &nbsp; -&gt; <a href="'.$data['urlAfter'].'">Nächster Eintrag</a>';
+      }
+      echo '<br>'."\n";
+    }
 
     // Postings
-    if ($anz_posts = count($art['posts'])) {
-      ?>
-      <br><br>
-      <div class="us">Kommentare</div>
-      <?
-      foreach ($art['posts'] as $post) {
+    if ($data['type'] == 'artikel') {
+      if ($anz_posts = count($data['posts'])) {
         ?>
-        <div style="margin:0px;border-color:#009999; border-width:1px; border-style:solid; padding:5px;">
-          <table style="border-width:0px; width:100%; margin:0;padding:0">
-          <tr>
-          <td style="padding:0px;">
-            #<?= $post['lfnr'] ?> von "<?= $post['username'] ?>":
-          </td>
-          <td style="padding:0px;text-align:right;">
-            <?= Date('Y-m-d H:i', strtotime($post['datum'])) ?>
-          </td>
-          </tr>
-          </table>
-          <?= $this->parse_post($post['text']) ?>
-        </div>
-        <div style="height:4px"></div>
+        <br><br>
+        <div class="us">Kommentare</div>
         <?
+        foreach ($data['posts'] as $post) {
+          ?>
+          <div style="margin:0px;border-color:#009999; border-width:1px; border-style:solid; padding:5px;">
+            <table style="border-width:0px; width:100%; margin:0;padding:0">
+            <tr>
+            <td style="padding:0px;">
+              #<?= $post['lfnr'] ?> von "<?= $post['username'] ?>":
+            </td>
+            <td style="padding:0px;text-align:right;">
+              <?= Date('Y-m-d H:i', strtotime($post['datum'])) ?>
+            </td>
+            </tr>
+            </table>
+            <?= $this->parse_post($post['text']) ?>
+          </div>
+          <div style="height:4px"></div>
+          <?
+        }
+        if ($anz_posts == 1) {
+          echo '1 Kommentar.';
+        } else {
+          echo $anz_posts.' Kommentare.';
+        }
+        echo '<br><br>'."\n";
       }
-      if ($anz_posts == 1) {
-        echo '1 Kommentar.';
-      } else {
-        echo $anz_posts.' Kommentare.';
-      }
-      echo '<br><br>'."\n";
+      ?>
+      
+      Mein Kommentar dazu:
+      <form method="post" action="../confirm.php">
+      <input type="hidden" name="aid" value="<?= $data['id'] ?>">
+      <div style="border-color:#009999; border-width:1px; border-style:solid; padding:10px; text-align:center;">
+      <table style="margin:0px;padding:0px;border-spacing:0px;border-collapse:collapse; border-width:0px;width:100%">
+      <tr>
+      <td style="width:30%;text-align:right;">
+        Name:
+      </td>
+      <td style="width:70%;text-align:left;">
+        <input type="text" name="username" value="">
+        (freiwillige Angabe)
+      </td>
+      </tr>
+      <tr>
+      <td style="text-align:right;">
+        E-Mail-Adresse:
+      </td>
+      <td style="text-align:left;">
+        <input type="email" name="usermail" value="@">
+        (Wird nirgends veröffentlicht, ist aber Pflichtangabe)
+      </td>
+      </tr>                                                               
+      </table>
+      <br>
+      Kommentar:
+      <br>
+      <textarea name="ptext" value="" rows="8" style="width:90%;"></textarea>
+      <br>
+      <input type="submit" value="Kommentar abschicken">
+      </div>
+      </form>
+      <?
     }
-    ?>
     
-    Mein Kommentar dazu:
-    <form method="post" action="../confirm.php">
-    <input type="hidden" name="aid" value="<?= $art['id'] ?>">
-    <div style="border-color:#009999; border-width:1px; border-style:solid; padding:10px; text-align:center;">
-    <table style="margin:0px;padding:0px;border-spacing:0px;border-collapse:collapse; border-width:0px;width:100%">
-    <tr>
-    <td style="width:30%;text-align:right;">
-      Name:
-    </td>
-    <td style="width:70%;text-align:left;">
-      <input type="text" name="username" value="">
-      (freiwillige Angabe)
-    </td>
-    </tr>
-    <tr>
-    <td style="text-align:right;">
-      E-Mail-Adresse:
-    </td>
-    <td style="text-align:left;">
-      <input type="email" name="usermail" value="@">
-      (Wird nirgends veröffentlicht, ist aber Pflichtangabe)
-    </td>
-    </tr>                                                               
-    </table>
-    <br>
-    Kommentar:
-    <br>
-    <textarea name="ptext" value="" rows="8" style="width:90%;"></textarea>
-    <br>
-    <input type="submit" value="Kommentar abschicken">
-    </div>
-    </form>
-    <?
-
-    $this->foot($ddata['navi_arts']);
+    $this->foot($data['navi_arts']);
   }
 
   
   /**
    * Artikel für die Web-Anzeige parsen
-   * @access public for tests
+   * @access public for tests only
    */
   public function parseArtikel($text, $bilder, $videos) {
     // Zeilenumbrüche
