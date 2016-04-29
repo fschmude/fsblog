4a63f793e03815b1e2c95ae435e547353effb3f7
fschmude <mail@fritz-schmude.de>
2016-04-29 22:10:30 +0200

    .

---
+++ tests/MSnippet_Test.php
@@ -0,0 +1,31 @@
+<?
+require_once 'Testcase.php';
+require_once 'M/MSnippet.php';
+
+class MSnippet_Test extends Testcase {
+  
+  public function test_01() {
+    $m = new MSnippet();
+    $this->assertNotNull($m);
+  }
+  
+  
+  /*
+   * tests for getSnippet()
+   */
+  public function test_gs1() {
+    // error, falls sid ungültig
+    $m = new MSnippet();
+    try {
+      $m->getSnippet(0);
+      $thrown = false;
+    } catch (Exception $e) {
+      $this->assertSame('Keine sid angegeben.', $e->getMessage());
+      $thrown = true;
+    }
+    $this->assertTrue($thrown, 'Error nicht ausgelöst');
+  }
+  
+  
+}
+
