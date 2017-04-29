f0f95bbe6468b30d39ec35c48e6adb28d24b270c
fschmude <mail@fritz-schmude.de>
2017-04-29 21:24:43 +0200

    .

---
+++ private/V/VAdminStart.php
@@ -0,0 +1,31 @@
+<?
+/**
+ * View of all articles
+ */
+require_once PATH_PRIVATE.'V/VAdmin.php';
+
+class VAdminStart extends VAdmin {
+  
+  public function display($errmsg, $data) {
+    
+    $this->displayHead($errmsg, 'Backend Login', false);
+    
+    if (isset($data['msg']) && $msg = trim($data['msg'])) {
+      echo $msg.'<br>';
+    }
+    ?>
+    <form method="post" action="admin.php">
+    <input type="password" name="pass" id="pass" value="">
+    <button type="submit">Login</button>
+    </form>
+    <br>
+    
+    <script type="text/javascript">
+    var pw = document.getElementById('pass');
+    pw.focus();
+    </script>
+    <?
+    $this->displayFoot();
+  }
+
+}
