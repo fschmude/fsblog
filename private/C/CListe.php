2db93104109998186977ed2fdd45e17030248f87
fschmude <mail@fritz-schmude.de>
2017-04-29 21:24:43 +0200

    .

---
+++ private/C/CListe.php
@@ -0,0 +1,31 @@
+<?
+require_once PATH_PRIVATE.'C/Controller.php';
+
+class CListe extends Controller {
+  
+  public function work($get, $post, $files) {
+    try {
+      $errmsg = '';
+      $vdata = array('arts' => array(), 'page' => 'index');
+      $mart = $this->getObject('MArtikel');
+      
+      if (isset($get['list']) && $get['list'] == 'alle') {
+        // Liste aller Artikel
+        $vdata['arts'] = $mart->getAllLive();
+        $vdata['page'] = 'alle';
+        
+      } else {
+        // Startseite, nur neueste Artikel
+        $vdata['arts'] = $mart->getTop(5, true);
+      }
+      
+    } catch (Exception $e) {
+      $errmsg = $this->handleError($e);
+    }
+    
+    // display
+    $vliste = $this->getObject('VListe');
+    $vliste->display($errmsg, $vdata);
+  }
+  
+}
