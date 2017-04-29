147b0fb33d95e28f77489d33d7f50540cd2a2b93
fschmude <mail@fritz-schmude.de>
2017-04-29 21:24:42 +0200

    .

---
+++ private/C/CAdmin.php
@@ -0,0 +1,100 @@
+<?
+/**
+ * Controller für das gesamte Backend
+ */
+require_once PATH_PRIVATE.'C/Controller.php';
+
+class CAdmin extends Controller {
+  
+  public function work($get, $post, $files) {
+    $data = '';
+    $errmsg = '';
+    try {
+      $v = $this->getObject('VAdminStart');
+      
+      // Anmeldung überprüfen!
+      if (isset($get['action']) && $get['action'] == 'logout') {
+        $_SESSION['ok'] = false;
+        $data = array('msg' => 'Sie sind nun abgemeldet. Bitte Passwort eingeben.');
+      } elseif (!isset($_SESSION['ok']) || !$_SESSION['ok']) {
+        $pass = isset($post['pass']) ? $post['pass'] : '';
+        if (!$pass) {
+          $data = array('msg' => 'Bitte Passwort eingeben.');
+        } else {
+          
+          // last login try?
+          $strLastLogin = file_get_contents(LOGIN_FILE);
+          if (!$strLastLogin) {
+            throw new Exception('Letzter Login-Versuch konnte nicht gelesen werden.');
+          }
+          $dt = new DateTime($strLastLogin);
+          $dt->add(new DateInterval('PT'.LOGIN_REFRAK.'S'));
+          $dtnow = new DateTime();
+          $f = fopen(LOGIN_FILE, 'w');
+          fwrite($f, $dtnow->format('Y-m-d H:i:s'));
+          fclose($f);
+          if ($dtnow < $dt) {
+            $data = array('msg' => 'Bitte nur ein Login-Versuch alle '.LOGIN_REFRAK.' Sekunden.');
+          } elseif (md5($pass) != BACKEND_PASS_HASH) {
+            $data = array('msg' => 'Falsches Passwort.');
+          } else {
+            $_SESSION['ok'] = true;
+          }
+        }
+      }
+      
+      if (!isset($_SESSION['ok']) || !$_SESSION['ok']) {
+        // v ist schon AdminStart
+        
+      } else {
+        if (!isset($post['mode']) || !$mode = $post['mode']) {
+          $mode = 'Artikel_list';
+        }
+        
+        $aAction = explode('_', $mode);
+        if (count($aAction) == 2) {
+          $objtype = $aAction[0];
+          $act = $aAction[1];
+          $mclass = 'M'.$objtype;
+          $model = $this->getObject($mclass);
+          $vclass = 'VAdmin'.$objtype;
+          $filter = isset($post['filter']) ? (int) $post['filter'] : 0;
+          switch ($act) {
+          case 'del':
+            $model->delete($post['id']);
+            // fallthrough
+          case 'list':
+            $data = $model->getList($filter);
+            $v = $this->getObject($vclass.'List');
+            break;
+            
+          case 'new':
+            $id = $model->create($filter);
+            $data = $model->getItem($id);
+            $v = $this->getObject($vclass);
+            break;
+            
+          case 'up':
+            $model->edit($post, isset($files['datei']) ? $files['datei'] : array());
+            // fallthrough
+          case 'up1':
+            $data = $model->getItem($post['id']);
+            $v = $this->getObject($vclass);
+            break;
+            
+          default:
+            throw new Exception('Ungültiger mode: "'.$mode.'"');
+          }
+        }
+      }
+      
+    } catch (Exception $e) {
+      $errmsg = $this->handleError($e);
+      $v = $this->getObject('VAdminStart');
+    }
+    
+    // Now display our findings
+    $v->display($errmsg, $data);
+  }
+
+}
